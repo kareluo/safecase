383457442616d5a2f40e62038b43150394f6a9c5
kareluo <weexiu@163.com>
2017-12-03 18:55:13 +0800

    add core

---
+++ android-core/store/app/src/main/java/me/kareluo/safecase/core/Secret.java
@@ -0,0 +1,9 @@
+package me.kareluo.safecase.core;
+
+/**
+ * Created by felix on 2017/12/3 下午6:53.
+ */
+
+public class Secret {
+
+}
---
+++ android-core/store/app/src/main/java/me/kareluo/safecase/core/Secrets.java
@@ -0,0 +1,9 @@
+package me.kareluo.safecase.core;
+
+/**
+ * Created by felix on 2017/12/3 下午6:54.
+ */
+
+public class Secrets {
+    
+}
